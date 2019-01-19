1eeca760da04cd2c39d2b281dbe58e8a2651d15b
msh <miarutun1@yandex.ru>
2019-01-20 01:21:27 +0300

    dpdk test

---
+++ test-dpdk-ip_reassembly/Release/src/test-dpdk-ip_reassembly.d
@@ -0,0 +1,160 @@
+src/test-dpdk-ip_reassembly.o: ../src/test-dpdk-ip_reassembly.c \
+ /usr/include/dpdk/rte_common.h /usr/include/dpdk/rte_config.h \
+ /usr/include/dpdk/rte_byteorder.h \
+ /usr/include/dpdk/generic/rte_byteorder.h \
+ /usr/include/dpdk/rte_byteorder_64.h /usr/include/dpdk/rte_log.h \
+ /usr/include/dpdk/rte_memory.h /usr/include/dpdk/rte_memcpy.h \
+ /usr/include/dpdk/rte_vect.h /usr/include/dpdk/generic/rte_vect.h \
+ /usr/include/dpdk/rte_eal.h /usr/include/dpdk/rte_per_lcore.h \
+ /usr/include/dpdk/rte_bus.h /usr/include/dpdk/rte_dev.h \
+ /usr/include/dpdk/rte_pci_dev_feature_defs.h \
+ /usr/include/dpdk/rte_launch.h /usr/include/dpdk/rte_atomic.h \
+ /usr/include/dpdk/generic/rte_atomic.h /usr/include/dpdk/rte_atomic_64.h \
+ /usr/include/dpdk/rte_cycles.h /usr/include/dpdk/generic/rte_cycles.h \
+ /usr/include/dpdk/rte_debug.h /usr/include/dpdk/rte_log.h \
+ /usr/include/dpdk/rte_branch_prediction.h \
+ /usr/include/dpdk/rte_branch_prediction.h \
+ /usr/include/dpdk/rte_prefetch.h \
+ /usr/include/dpdk/generic/rte_prefetch.h /usr/include/dpdk/rte_lcore.h \
+ /usr/include/dpdk/rte_interrupts.h \
+ /usr/include/dpdk/rte_eal_interrupts.h /usr/include/dpdk/rte_random.h \
+ /usr/include/dpdk/rte_ether.h /usr/include/dpdk/rte_mbuf.h \
+ /usr/include/dpdk/rte_mempool.h /usr/include/dpdk/rte_spinlock.h \
+ /usr/include/dpdk/generic/rte_spinlock.h /usr/include/dpdk/rte_pause.h \
+ /usr/include/dpdk/generic/rte_pause.h /usr/include/dpdk/rte_rtm.h \
+ /usr/include/dpdk/rte_cpuflags.h \
+ /usr/include/dpdk/generic/rte_cpuflags.h /usr/include/dpdk/rte_common.h \
+ /usr/include/dpdk/rte_pause.h /usr/include/dpdk/rte_ring.h \
+ /usr/include/dpdk/rte_memzone.h /usr/include/dpdk/rte_mbuf_ptype.h \
+ /usr/include/dpdk/rte_ethdev.h /usr/include/dpdk/rte_devargs.h \
+ /usr/include/dpdk/rte_errno.h /usr/include/dpdk/rte_ether.h \
+ /usr/include/dpdk/rte_eth_ctrl.h /usr/include/dpdk/rte_dev_info.h \
+ /usr/include/dpdk/rte_malloc.h /usr/include/dpdk/rte_ip.h \
+ /usr/include/dpdk/rte_tcp.h /usr/include/dpdk/rte_udp.h \
+ /usr/include/dpdk/rte_string_fns.h /usr/include/dpdk/rte_lpm.h \
+ /usr/include/dpdk/rte_compat.h /usr/include/dpdk/rte_lpm_sse.h \
+ /usr/include/dpdk/rte_lpm6.h /usr/include/dpdk/rte_ip_frag.h
+
+/usr/include/dpdk/rte_common.h:
+
+/usr/include/dpdk/rte_config.h:
+
+/usr/include/dpdk/rte_byteorder.h:
+
+/usr/include/dpdk/generic/rte_byteorder.h:
+
+/usr/include/dpdk/rte_byteorder_64.h:
+
+/usr/include/dpdk/rte_log.h:
+
+/usr/include/dpdk/rte_memory.h:
+
+/usr/include/dpdk/rte_memcpy.h:
+
+/usr/include/dpdk/rte_vect.h:
+
+/usr/include/dpdk/generic/rte_vect.h:
+
+/usr/include/dpdk/rte_eal.h:
+
+/usr/include/dpdk/rte_per_lcore.h:
+
+/usr/include/dpdk/rte_bus.h:
+
+/usr/include/dpdk/rte_dev.h:
+
+/usr/include/dpdk/rte_pci_dev_feature_defs.h:
+
+/usr/include/dpdk/rte_launch.h:
+
+/usr/include/dpdk/rte_atomic.h:
+
+/usr/include/dpdk/generic/rte_atomic.h:
+
+/usr/include/dpdk/rte_atomic_64.h:
+
+/usr/include/dpdk/rte_cycles.h:
+
+/usr/include/dpdk/generic/rte_cycles.h:
+
+/usr/include/dpdk/rte_debug.h:
+
+/usr/include/dpdk/rte_log.h:
+
+/usr/include/dpdk/rte_branch_prediction.h:
+
+/usr/include/dpdk/rte_branch_prediction.h:
+
+/usr/include/dpdk/rte_prefetch.h:
+
+/usr/include/dpdk/generic/rte_prefetch.h:
+
+/usr/include/dpdk/rte_lcore.h:
+
+/usr/include/dpdk/rte_interrupts.h:
+
+/usr/include/dpdk/rte_eal_interrupts.h:
+
+/usr/include/dpdk/rte_random.h:
+
+/usr/include/dpdk/rte_ether.h:
+
+/usr/include/dpdk/rte_mbuf.h:
+
+/usr/include/dpdk/rte_mempool.h:
+
+/usr/include/dpdk/rte_spinlock.h:
+
+/usr/include/dpdk/generic/rte_spinlock.h:
+
+/usr/include/dpdk/rte_pause.h:
+
+/usr/include/dpdk/generic/rte_pause.h:
+
+/usr/include/dpdk/rte_rtm.h:
+
+/usr/include/dpdk/rte_cpuflags.h:
+
+/usr/include/dpdk/generic/rte_cpuflags.h:
+
+/usr/include/dpdk/rte_common.h:
+
+/usr/include/dpdk/rte_pause.h:
+
+/usr/include/dpdk/rte_ring.h:
+
+/usr/include/dpdk/rte_memzone.h:
+
+/usr/include/dpdk/rte_mbuf_ptype.h:
+
+/usr/include/dpdk/rte_ethdev.h:
+
+/usr/include/dpdk/rte_devargs.h:
+
+/usr/include/dpdk/rte_errno.h:
+
+/usr/include/dpdk/rte_ether.h:
+
+/usr/include/dpdk/rte_eth_ctrl.h:
+
+/usr/include/dpdk/rte_dev_info.h:
+
+/usr/include/dpdk/rte_malloc.h:
+
+/usr/include/dpdk/rte_ip.h:
+
+/usr/include/dpdk/rte_tcp.h:
+
+/usr/include/dpdk/rte_udp.h:
+
+/usr/include/dpdk/rte_string_fns.h:
+
+/usr/include/dpdk/rte_lpm.h:
+
+/usr/include/dpdk/rte_compat.h:
+
+/usr/include/dpdk/rte_lpm_sse.h:
+
+/usr/include/dpdk/rte_lpm6.h:
+
+/usr/include/dpdk/rte_ip_frag.h:
